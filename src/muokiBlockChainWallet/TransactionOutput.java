31441c6513246b0ea6b7f6646bfffe4b0f0de560
muokicaleb <muokicaleb@gmail.com>
2018-06-29 13:54:56 +0300

    finished Transaction*

--- src/muokiBlockChainWallet/TransactionOutput.java
+++ src/muokiBlockChainWallet/TransactionOutput.java
@@ -1,4 +1,24 @@
 package muokiBlockChainWallet;
 
+import java.security.PublicKey;
+
 public class TransactionOutput {
-}
+    public String id;
+    public PublicKey recipient; //also known as the new owner of these coins.
+    public float value; //the amount of coins they own
+    public String parentTransactionId; //the id of the transaction this output was created in
+
+    //Constructor
+    public TransactionOutput(PublicKey recipient, float value, String parentTransactionId) {
+        this.recipient = recipient;
+        this.value = value;
+        this.parentTransactionId = parentTransactionId;
+        this.id = StringUtil.applySha256(StringUtil.getStringFromKey(recipient)+Float.toString(value)+parentTransactionId);
+    }//end TransactionOutput constructor
+
+    //Check if coin belongs to you
+    public boolean isMine(PublicKey publicKey) {
+        return (publicKey == recipient);
+    }
+
+}// end class TransactionOutput

3206150eebcdaa23f7b5ceed246e71596b3000ad
muokicaleb <muokicaleb@gmail.com>
2018-06-29 14:11:08 +0300

    finished

--- src/muokiBlockChainWallet/Transaction.java
+++ src/muokiBlockChainWallet/Transaction.java
@@ -1,5 +1,6 @@
 package muokiBlockChainWallet;
 
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.ArrayList;
 
@@ -24,6 +25,73 @@ public class Transaction
         this.inputs = inputs;
 
     }//end constructor
+
+    public boolean processTransaction() {
+
+        if(verifySignature() == false) {
+            System.out.println("#Transaction Signature failed to verify");
+            return false;
+        }
+
+        //Gathers transaction inputs (Making sure they are unspent):
+        for(TransactionInput i : inputs) {
+            i.UTXO = Main.UTXOs.get(i.transactionOutputId);
+        }
+
+        //Checks if transaction is valid:
+        if(getInputsValue() < Main.minimumTransaction) {
+            System.out.println("Transaction Inputs too small: " + getInputsValue());
+            System.out.println("Please enter the amount greater than " + Main.minimumTransaction);
+            return false;
+        }
+
+        //Generate transaction outputs:
+        float leftOver = getInputsValue() - value; //get value of inputs then the left over change:
+        transactionID = calculateHash();
+        outputs.add(new TransactionOutput( this.recipient, value,transactionID)); //send value to recipient
+        outputs.add(new TransactionOutput( this.sender, leftOver,transactionID)); //send the left over 'change' back to sender
+
+        //Add outputs to Unspent list
+        for(TransactionOutput o : outputs) {
+            Main.UTXOs.put(o.id , o);
+        }
+
+        //Remove transaction inputs from UTXO lists as spent:
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it
+            Main.UTXOs.remove(i.UTXO.id);
+        }
+
+        return true;
+    }
+
+    public float getInputsValue() {
+        float total = 0;
+        for(TransactionInput i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it, This behavior may not be optimal.
+            total += i.UTXO.value;
+        }
+        return total;
+    }
+
+    public void generateSignature(PrivateKey privateKey) {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Float.toString(value)	;
+        signature = StringUtil.applyECDSASig(privateKey,data);
+    }
+
+    public boolean verifySignature() {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient) + Float.toString(value)	;
+        return StringUtil.verifyECDSASig(sender, data, signature);
+    }
+
+    public float getOutputsValue() {
+        float total = 0;
+        for(TransactionOutput o : outputs) {
+            total += o.value;
+        }
+        return total;
+    }
+
     //calculate the transaction hash which will be used as its Id
     private String calculateHash()
     {
--- src/muokiBlockChainWallet/Wallet.java
+++ src/muokiBlockChainWallet/Wallet.java
@@ -1,12 +1,16 @@
 package muokiBlockChainWallet;
 
-import  java.security.*;
+import java.security.*;
 import java.security.spec.ECGenParameterSpec;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 public class Wallet
 {
     public PrivateKey privateKey;
     public PublicKey publicKey;
+
+    public HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>();
 
     public Wallet()
     {
@@ -34,4 +38,41 @@ public class Wallet
              throw new RuntimeException(e);
          }//end catch
     }//end method generateKeyPair
+    public float getBalance() {
+        float total = 0;
+        for (Map.Entry<String, TransactionOutput> item: Main.UTXOs.entrySet()){
+            TransactionOutput UTXO = item.getValue();
+            if(UTXO.isMine(publicKey)) { //if output belongs to me ( if coins belong to me )
+                UTXOs.put(UTXO.id,UTXO); //add it to our list of unspent transactions.
+                total += UTXO.value ;
+            }
+        }
+        return total;
+    }
+
+    public Transaction sendFunds(PublicKey _recipient,float value ) {
+        if(getBalance() < value) {
+            System.out.println("#Not Enough funds to send transaction. Transaction Discarded.");
+            return null;
+        }
+        ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+
+        float total = 0;
+        for (Map.Entry<String, TransactionOutput> item: UTXOs.entrySet()){
+            TransactionOutput UTXO = item.getValue();
+            total += UTXO.value;
+            inputs.add(new TransactionInput(UTXO.id));
+            if(total > value) break;
+        }
+
+        Transaction newTransaction = new Transaction(publicKey, _recipient , value, inputs);
+        newTransaction.generateSignature(privateKey);
+
+        for(TransactionInput input: inputs){
+            UTXOs.remove(input.transactionOutputId);
+        }
+
+        return newTransaction;
+    }
+
 }// end class Wallet
